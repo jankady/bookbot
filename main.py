f8d094979a79812ae3eeef4cf78d66ca32801a8f
jankady <kaduch05@gmail.com>
2025-08-26 16:53:29 +0200

    make Report string for book

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from stats import count_words, count_characters
+from stats import *
 
 def get_book_text(filename):
     with open(filename, encoding="utf-8") as f:
         return f.read()
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    char_num = count_characters(book_text)
-    print(char_num)
+    path = "books/frankenstein.txt"
 
+    book_text = get_book_text(path)
 
+    books = sort_dict(count_characters(book_text))
+
+    books.sort(reverse=True, key=sort_on)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(book_text)} total words")
+    print("--------- Character Count -------")
+    for book in books:
+        if book['char'].isalpha():
+            print(book)
+    print("============= END ===============")
 main()
